a673ae7da079da58766b0b6f07e55ba5ce9e6049
aiden00713 <44128720+aiden00713@users.noreply.github.com>
2019-08-04 23:05:10 +0800

    Update 左下三角形.c

--- 左下三角形.c
+++ 左下三角形.c
@@ -1,9 +1,9 @@
 #include <Stdio.h>
-//左下三角形 
-int main(void){
-	int n,i=0,j=0;
+int main(void)
+{
+	int n=0,i=0,j=0;
 	scanf("%d",&n);
-	for(i=0;i<n;i++)  //行 
+	for(i=0;i<n;i++) 
 	{
 		for(j=n;j>i;j--) 
 		{
